ea37316b83ac431c5f97455527826743b160cb92
adoxic <griffinmaeve@gmail.com>
2020-01-18 14:14:28 -0800

    images are being added to array extra info is still included must filter in page links

--- she-ra-scraper.js
+++ she-ra-scraper.js
@@ -1,12 +1,25 @@
 // she-ra page test
 
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const url = 'https://she-raandtheprincessesofpower.fandom.com/wiki/Adora';
 
 axios(url)
   .then(response => {
     const html = response.data;
-    console.log(html);
+    const $ = cheerio.load(html);
+    const characterImages = $('.image.image-thumbnail');
+
+    const character = {
+      images: [],
+    };
+    for (let i = 0; i < characterImages.length; i++) {
+      const image = characterImages[i].attribs;
+      if(image.title) {
+        character.images[i] = image;
+      }
+    }
+    console.log(character);
   })
   .catch(console.error);

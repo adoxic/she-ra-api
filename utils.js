c2ee95fc5b8f48d5a7c23fc1c28085389d154421
adoxic <griffinmaeve@gmail.com>
2020-01-20 17:46:29 -0800

    fixed formatting of lists

--- utils.js
+++ utils.js
@@ -2,8 +2,11 @@ const formatList = (arr, type) => {
   for (let i = 0; i < arr.length; i++) {
     const elementObj = arr[i];
 
-    if(elementObj.data) {
-      elementObj.data = elementObj.data.replace(/\(|\)/g, '');
+    if(elementObj.type === 'tag' && elementObj.attribs.title) {
+      linkName = elementObj.attribs.title;
+      const joined = `${arr[i - 1].data}${linkName}${arr[i + 1].data}`;
+      type.splice((type.length -1), 1, joined);
+      i++;
     }
 
     if(elementObj.data) {

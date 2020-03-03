b2541c9eabbb1d2294f11caed42d69b9cdadd496
adoxic <griffinmaeve@gmail.com>
2020-03-02 17:17:45 -0800

    get is problematic

--- e2e/__tests__/characters.test.js
+++ e2e/__tests__/characters.test.js
@@ -16,35 +16,46 @@ describe('test character routes', () => {
     return mongoose.connection.close();
   });
 
+  const adora = {
+    name: 'Adora',
+    images: [
+      {
+        href: 'https://vignette.wikia.nocookie.net/shera-and-the-princesses-of-power/images/a/a4/AdoraRender.png/revision/latest?cb=20181030172527',
+        class: 'image image-thumbnail ',
+        title: 'Adora'
+      },
+      {
+        href: 'https://vignette.wikia.nocookie.net/shera-and-the-princesses-of-power/images/b/b4/She-RaRender.png/revision/latest?cb=20181108162238',
+        class: 'image image-thumbnail ',
+        title: 'She-Ra'
+      }
+    ],
+    Skills: [
+      'Super-strength',
+      'Shapeshifting Sword',
+      'Limited healing powers'
+    ],
+    Status: 'Alive'
+  };
+
   it('should post a character', () => {
-    const data = {
-      name: 'Adora',
-      images: [
-        {
-          href: 'https://vignette.wikia.nocookie.net/shera-and-the-princesses-of-power/images/a/a4/AdoraRender.png/revision/latest?cb=20181030172527',
-          class: 'image image-thumbnail ',
-          title: 'Adora'
-        },
-        {
-          href: 'https://vignette.wikia.nocookie.net/shera-and-the-princesses-of-power/images/b/b4/She-RaRender.png/revision/latest?cb=20181108162238',
-          class: 'image image-thumbnail ',
-          title: 'She-Ra'
-        }
-      ],
-      Skills: [
-        'Super-strength',
-        'Shapeshifting Sword',
-        'Limited healing powers'
-      ],
-      Status: 'Alive'
-    };
 
     return request
       .post('/api/characters')
-      .send(data)
+      .send(adora)
       .expect(200)
       .then(({ body }) => {
         expect(body.name).toBe('Adora');
       });
+  });
+
+  it('should get a character', () => {
+    return request
+      .post('/api/characters')
+      .send(adora)
+      .then(({ body }) => {
+        expect(body.name).toBe('Adora');
+      });
+    
   });
 });
